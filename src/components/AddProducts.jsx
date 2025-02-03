4aeb45ca258e3e314609def09bdb153f3f603b02
ansanmk <ansanmk710140@gmail.com>
2025-02-03 23:03:11 +0530

    data binding shopping cart

--- src/components/AddProducts.jsx
+++ src/components/AddProducts.jsx
@@ -4,8 +4,9 @@ import NavBar from './NavBar'
 const AddProducts = () => {
     return (
         <div>
+             <NavBar /><br></br>
             <div className="container">
-                <NavBar /><br></br>
+               
                 <div className="row">
                     <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
 
--- src/components/ViewAllProducts.jsx
+++ src/components/ViewAllProducts.jsx
@@ -11,9 +11,9 @@ const ViewAllProducts = () => {
 
     return (
         <div>
-
+ <NavBar /><br></br>
             <div className="container">
-            <NavBar /><br></br>
+           
                 <div className="row">
                     <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
 
